104085629383d8908ca44dff074ae304505df95e
Ferran <43911200+fdomenech9@users.noreply.github.com>
2020-04-10 12:36:19 +0200

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,4 +1,16 @@
-CREATE VIEW supermarket.last_four_hours AS 
-SELECT idshop, groupname, unixtime, positives, negatives
-FROM supermarket.infoshop
-where unixtime between floor(extract(epoch from now())-14400) and floor(extract(epoch from now()));
+-- View: supermarket.last_four_hours
+
+-- DROP VIEW supermarket.last_four_hours;
+
+CREATE OR REPLACE VIEW supermarket.last_four_hours
+ AS
+ SELECT infoshop.idshop,
+    infoshop.groupname,
+    infoshop.unixtime,
+    infoshop.positives,
+    infoshop.negatives
+   FROM supermarket.infoshop
+  WHERE infoshop.unixtime::double precision >= floor(date_part('epoch'::text, now()) - 14400::double precision);
+
+ALTER TABLE supermarket.last_four_hours
+    OWNER TO postgres;
